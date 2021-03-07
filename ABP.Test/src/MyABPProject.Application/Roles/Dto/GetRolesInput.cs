f2c06590f2dd38b7627100d6b42eee98baa8ef58
魏天华 <witeem@126.com>
2021-03-07 12:24:10 +0800

    初始化运行成功

--- ABP.Test/src/MyABPProject.Application/Roles/Dto/GetRolesInput.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MyABPProject.Roles.Dto
-{
-    public class GetRolesInput
-    {
-        public string Permission { get; set; }
-    }
-}
--- ABP.Test/src/MyABPProject.Web.Core/Authentication/External/IExternalAuthConfiguration.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace MyABPProject.Authentication.External
-{
-    public interface IExternalAuthConfiguration
-    {
-        List<ExternalLoginProviderInfo> Providers { get; }
-    }
-}
